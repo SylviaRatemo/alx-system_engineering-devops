a6fe6e7cdd6657f694d8df2050cd3fb0c2a790e2
SylviaRatemo <ratemosylvia@gmail.com>
2023-08-08 05:05:31 -0700

    subsrciber count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Get number of subscribers'''
+import requests
+
+
+
+def number_of_subscribers(subreddit):
+    '''Gets number of reddit subscribers'''
+    headers = {'User-agent': 'Unix:0-subs:v1'}
+    response = requests.get('http://reddit.com/r/{}/about.json'
+                            .format(subreddit), headers=headers)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data', {}).get('subscribers', 0)
